eac540cf580f6f87e78c3adadea608898bb00b3a
Ariel Karlen <ariel.karlen@possumus.tech>
2022-02-02 09:13:24 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,10 @@
+function Formulario() {
+    return(
+        <>
+        <h1>Formulario</h1>
+        <p>Continua en video 54</p>
+        </>
+    )
+}
+
+export default Formulario;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function ListadoPacientes() {
+    return(
+        <div>
+            <h2>Listado Pacientes</h2>
+        </div>
+    )
+}
+
+export default ListadoPacientes;
